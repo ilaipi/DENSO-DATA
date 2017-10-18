03979d147df4236731ed03bde4d7d6a800027d46
Billy Yang <mz.yyam@gmail.com>
2017-10-18 21:27:44 +0800

    采集近3个月数据

--- src/common/initial/index.js
+++ src/common/initial/index.js
@@ -1,5 +1,11 @@
-import sfe from './../../modules/sfe/service.js';
+import moment from 'moment';
+
+import * as sfe from './../../modules/sfe/service.js';
 
 export default async () => {
-  await sfe();
+  let date = moment().subtract(3, 'months');
+  do {
+    await sfe.gather(date.format('YYYYMMDD'));
+    date.add(1, 'days');
+  } while (date.isBefore(moment()));
 };
