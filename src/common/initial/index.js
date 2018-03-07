d5c3e198cf1b166d4ceda7f7fe8afe8bdcdc105a
Billy Yang <mz.yyam@gmail.com>
2018-03-07 15:57:57 +0800

    重跑sfe 2018-02-12以后的数据

--- src/common/initial/index.js
+++ src/common/initial/index.js
@@ -4,13 +4,13 @@
 import moment from 'moment';
 
 import * as sfe from './../../modules/sfe/service.js';
-import * as ccmn from './../../modules/ccmn/service.js';
+// import * as ccmn from './../../modules/ccmn/service.js';
 
 export default async () => {
-  let date = moment(new Date('2017-11-16'));
+  let date = moment(new Date('2018-02-12'));
   do {
     await sfe.gather(date.format('YYYYMMDD'));
     date.add(1, 'days');
   } while (date.isBefore(moment()));
-  await ccmn.gather();
+  // await ccmn.gather();
 };
